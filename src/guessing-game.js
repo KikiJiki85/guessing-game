2dca920703279db79bc307523162931b6bd0a80f
Aleksey Vaskin <alzor85@gmail.com>
2023-08-31 08:28:03 +0300

    feat: finish guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,31 @@
 class GuessingGame {
     constructor() {}
 
-    setRange(min, max) {
+    arr = [];
+    left = 0; 
+    right = 0;
+    mid = 0;
 
+    setRange(min, max) {
+        for(let i = min; i <= max; i++) {
+            this.arr.push(i);
+        }
+        this.right = this.arr.length - 1; 
     }
 
-    guess() {
-
+    guess() {     
+        this.mid = Math.round((this.right + this.left) / 2);
+        return this.mid;
     }
 
     lower() {
-
+        this.right = this.mid;
+        return this.guess();
     }
 
     greater() {
-
+        this.left = this.mid;
+        return this.guess();
     }
 }
 
